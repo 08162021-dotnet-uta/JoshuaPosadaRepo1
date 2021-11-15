14c711090e3ceba4cd1313cf7e8498495e194a6c
Joshua Posada <xallies@gmail.com>
2021-11-15 16:42:52 -0500

    week2 javascript and c#

---
+++ onboard/week2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace week2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+
+      Console.WriteLine(ReverseCase("Happy Birthday"));
+
+
+      Console.WriteLine(ReverseCase("MANY THANKS"));
+
+
+      Console.WriteLine(ReverseCase("sPoNtAnEoUs"));
+    }
+    public static string ReverseCase(string inputstring)
+    {
+      StringBuilder sb = new StringBuilder();
+      foreach (char x in inputstring)
+      {
+        if (Char.IsUpper(x))
+        {
+
+          sb.Append(Char.ToLower(x));
+        }
+        else
+        {
+          sb.Append(Char.ToUpper(x));
+        }
+
+      }
+
+      return sb.ToString();
+
+    }
+  }
+}
---
+++ onboard/week2/twofunctions.js
@@ -0,0 +1,41 @@
+function getConsonantSubstrings() {
+  var str = arguments[0];
+  let vowels = new Array('a', 'e', 'i', 'o', 'u');
+  let Aarray = new Array();
+  for (var i = 0; i < str.length; i++) {
+    if (!vowels.includes(str[i])) {
+      if (!Aarray.includes(str[i]))
+        Aarray.push(str[i]);
+      for (var j = str.length; j > i; j--) {
+        if (!vowels.includes(str[j])) {
+          if (!Aarray.includes(str.substring(i, j + 1)))
+            Aarray.push(str.substring(i, j + 1));
+        }
+      }
+    }
+
+  }
+  Aarray = Aarray.sort();
+  return Aarray;
+}
+
+function getVowelSubstrings() {
+  var str = arguments[0];
+  let vowels = new Array('a', 'e', 'i', 'o', 'u');
+  let Aarray = new Array();
+  for (var i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      if (!Aarray.includes(str[i]))
+        Aarray.push(str[i]);
+      for (var j = str.length; j > i; j--) {
+        if (vowels.includes(str[j])) {
+          if (!Aarray.includes(str.substring(i, j + 1)))
+            Aarray.push(str.substring(i, j + 1));
+        }
+      }
+    }
+
+  }
+  Aarray.sort();
+  return Aarray;
+}
